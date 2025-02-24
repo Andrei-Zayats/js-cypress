219a2f877a1e348841c82682cb11fa683bd8e1da
Andrei Zayats <andrei_zayats@outlook.com>
2025-02-24 23:28:10 +0100

    Add add-remove-elements tests

---
+++ cypress/e2e/the-internet/add-remove-elements.cy.js
@@ -0,0 +1,33 @@
+describe("Add/Remove Elements Page", () => {
+
+  const addElementButton = ".example>button";
+  const deleteButtons = "#elements button";
+
+  beforeEach(() => {
+    cy.visit("https://the-internet.herokuapp.com/add_remove_elements/");
+  });
+
+  it("add elements", () => {
+    cy.get(addElementButton).should("be.visible");
+    cy.get(addElementButton).click();
+    cy.get(addElementButton).click();
+    cy.get(addElementButton).click();
+    cy.get(deleteButtons).should("have.length", 3);
+  });
+
+  it("delete elements", () => {
+    cy.get(addElementButton).should("be.visible");
+    cy.get(addElementButton).click();
+    cy.get(addElementButton).click();
+    cy.get(addElementButton).click();
+    cy.get(deleteButtons).should("have.length", 3);
+    cy.get(deleteButtons).last().click();
+    cy.get(deleteButtons).should("have.length", 2);
+    cy.get(deleteButtons).first().click();
+    cy.get(deleteButtons).should("have.length", 1);
+  });
+
+  it.skip("dummy test", () => {
+    cy.get(addElementButton).should("not.be.visible");
+  });
+});
